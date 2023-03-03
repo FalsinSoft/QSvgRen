6b8fb499ac035f9e97f4430ebfd8441dc27706cb
FalsinSoft <falsinsoft@gmail.com>
2023-03-03 21:35:06 +0100

    Changed library compilation from static to shared

--- QSvgRen/QSvgRen.h
+++ QSvgRen/QSvgRen.h
@@ -3,7 +3,21 @@
 #include <QQuickImageProvider>
 #include <QImage>
 
-class QSvgRen : public QQuickImageProvider
+#ifdef QSVGREN_DLL
+#  ifndef QSVGREN_EXPORT
+#    ifdef QSVGREN_EXPORT_LIBRARY
+#      define QSVGREN_EXPORT Q_DECL_EXPORT
+#    else
+#      define QSVGREN_EXPORT Q_DECL_IMPORT
+#    endif
+#  endif
+#else
+#  ifndef QSVGREN_EXPORT
+#    define QSVGREN_EXPORT
+#  endif
+#endif
+
+class QSVGREN_EXPORT QSvgRen : public QQuickImageProvider
 {
 public:
     QSvgRen();
